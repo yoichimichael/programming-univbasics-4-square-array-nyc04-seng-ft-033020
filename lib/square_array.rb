ddbd8cc4bb088364a3f265dcaba71e5328b3895e
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-22 06:13:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   squared = []
   counter = 0 
   while array[counter] do
-    squared << array[counter]**array[counter]
+    squared << array[counter] ** 2
+    counter += 1
   end
   squared
 end
